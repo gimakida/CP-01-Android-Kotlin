a0349429f302985ef3ae3cd814950c000a5b3410
gimakida <giovanna.makida@gmail.com>
2025-09-03 21:06:39 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val quaNumber = findViewById<EditText>(R.id.quaNumber)
+
+
+    }
+    }
